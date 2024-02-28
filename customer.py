f8839ffd891ae2bae6abc3f702001208fbe4d323
Macan Mehri <macanmehri@gmail.com>
2024-02-29 00:15:00 +0330

    Added show_purchased_list method and override str method in customers class

--- customer.py
+++ customer.py
@@ -15,8 +15,25 @@ class Customer:
         self.last_name = last_name
         self.birth_year = birth_year
         self.customer_id = customer_id
+        # A list of dictionaries that contains product id and purchased time
         self.bought_products = list()
 
+
+    def __str__(self) -> str:
+        """Override string method"""
+        message = (
+            f'Name: {self.full_name}\nBirthday: {self.birth_year}\n'
+            f'ID: {self.customer_id}'
+        )
+        return message
+
+
+    def show_purchased_list(self) -> None:
+        """
+        Showing products list that customer has purchased
+        """
+        for product in self.bought_products:
+            print(f'{product['Product']} : {str(product['Date'])}')
 
     @property
     def full_name(self) -> str:
@@ -34,14 +51,14 @@ class Customer:
         return CURRENT_YEAR - self.birth_year
 
 
-    def buy_product(self, product_id: int, date: jdatetime.date):
+    def buy_product(self, product_name: str, date: jdatetime.date):
         """
         Customer buys a product
         Includes id of product and time that customer bought product
         """
         self.bought_products.append(
             {
-                'Product': product_id,
+                'Product': product_name,
                 'Date': date
             }
         )
@@ -88,4 +105,21 @@ class Customer:
 
 if __name__ == '__main__':
 
-    pass
+    c1 = Customer(
+        first_name='Macan',
+        last_name='Mehri',
+        birth_year=1380,
+        customer_id=1
+    )
+
+    c1.buy_product(
+        product_name='Pen',
+        date=jdatetime.datetime.now().date()
+    )
+
+    c1.buy_product(
+        product_name='Book',
+        date=jdatetime.date(day=22, month=11, year=1400)
+    )
+
+    c1.show_purchased_list()
