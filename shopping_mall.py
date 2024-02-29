4215977b204c8faa20e2a150f3f842d42e152cdf
Macan Mehri <macanmehri@gmail.com>
2024-02-29 16:16:04 +0330

    instead of time.now for customers purchased date time, get the date time from user

--- shopping_mall.py
+++ shopping_mall.py
@@ -50,13 +50,13 @@ class ShoppingMall:
         )
 
 
-    def customer_buy_product(self, customer: Customer, product: Product):
+    def customer_buy_product(self, customer: Customer, product: Product, date: jdatetime.date):
         """
         A customer purchases a product
         """
         customer.buy_product(
             product=product,
-            date=jdatetime.datetime.now().date()
+            date=date
         )
         self.sell_product(product=product)
 
@@ -73,68 +73,4 @@ class ShoppingMall:
 
 
 if __name__ == '__main__':
-    mall = ShoppingMall()
-
-    customer1 = Customer(
-        first_name='Macan',
-        last_name='Mehri',
-        birth_year=1380,
-        customer_id=1
-    )
-    customer2 = Customer(
-        first_name='Amir',
-        last_name='Mehri',
-        birth_year=1381,
-        customer_id=2
-    )
-    product1 = Product(
-        product_name='Pen',
-        product_id=1,
-        product_price=35_000
-    )
-    product2 = Product(
-        product_name='Book',
-        product_id=2,
-        product_price=100_000
-    )
-    product3 = Product(
-        product_name='Table',
-        product_id=3,
-        product_price=1_000_000
-    )
-
-
-    mall.add_customer(customer=customer1)
-    mall.add_customer(customer=customer2)
-    mall.add_product(product=product1)
-    mall.add_product(product=product2)
-    mall.add_product(product=product3)
-
-    mall.customer_buy_product(
-        customer=mall.customers[0],
-        product=mall.products[0]
-    )
-    mall.customer_buy_product(
-        customer=mall.customers[0],
-        product=mall.products[1]
-    )
-    mall.customer_buy_product(
-        customer=mall.customers[0],
-        product=mall.products[2]
-    )
-    mall.customer_buy_product(
-        customer=mall.customers[0],
-        product=mall.products[2]
-    )
-    mall.customer_buy_product(
-        customer=mall.customers[0],
-        product=mall.products[2]
-    )
-    mall.customer_buy_product(
-        customer=mall.customers[0],
-        product=mall.products[2]
-    )
-
-    for purchase in mall.purchases:
-        print(purchase['Product'])
-        print(f'Number of purchases: {purchase['Purchases']}')
+    pass
