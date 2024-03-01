04a5bac068565b1a4af0d097611b6632dab94580
Macan Mehri <macanmehri@gmail.com>
2024-03-01 16:14:05 +0330

    Created a menu for user

--- main.py
+++ main.py
@@ -68,6 +68,23 @@ def change_product_price(product: Product, new_price: float, date: jdatetime.dat
     product.change_product_price(new_price=new_price, date=date)
 
 
+def change_prices_random(mall: ShoppingMall) -> None:
+    """Change products prices random"""
+    for i in range(10_000):
+        product = random.choice(mall.products)
+        month = random.randint(1, 12)
+        change_product_price(
+            product=product,
+            new_price=random.randint(100_000, 5_000_000),
+            date=jdatetime.date(year=CURRENT_YEAR, month=month, day=1)
+        )
+
+
+def each_month_price(product: Product) -> list:
+    """See each month purchase of a product"""
+    return product.price_changes_in_year()
+
+
 def customer_buy_random(mall: ShoppingMall) -> None:
     """Random customers buy random products"""
     for i in range(10_000):
@@ -89,7 +106,17 @@ def customer_buy_random(mall: ShoppingMall) -> None:
         )
 
 
-def each_month(product: str, mall: ShoppingMall) -> None:
+def customer_purchases(customer: Customer, mall: ShoppingMall) -> list:
+    """Customers purchases in a year"""
+    final_result = [0] * 12
+    for product in mall.products:
+        result = customer.purchase_each_month_of_year(product=product)
+        final_result = list(map(add, final_result, result))
+
+    return final_result
+
+
+def each_month(product: Product, mall: ShoppingMall) -> list:
     """See each month purchase of a product"""
     final_result = [0] * 12
     for customer in mall.customers:
@@ -99,38 +126,91 @@ def each_month(product: str, mall: ShoppingMall) -> None:
     return final_result
 
 
-def create_purchase_diagram(purchases: list, time: list):
+def create_diagram(purchases: list, time: list, ylabel: str):
     """
     Create a diagram of purchases and times
     """
     plt.bar(time, purchases, color='#47071e')
     plt.xlabel('Month')
-    plt.ylabel('Purchases')
+    plt.ylabel(ylabel)
 
 
 if __name__ == '__main__':
-    # Create random customers
+    # Create a shopping mall
     shopping_mall = ShoppingMall()
+    # Create random products
     create_products(mall=shopping_mall)
+    # Create random customers
     create_random_customers(mall=shopping_mall)
+    # Customers buy random products
     customer_buy_random(mall=shopping_mall)
+    # Change prices randomlly
+    change_prices_random(mall=shopping_mall)
 
-    #mall.show_customers()
-    #mall.show_products()
-    shopping_mall.show_purchases()
 
-    product_name = input(
-        'Please enter a product name to see products sells each month for a year: '
+    print(
+        '1. Show all customers.\n'
+        '2. Show all products.\n'
+        '3. Show purchased products.\n'
+        '4. Find a customer by id.\n'
+        '5. Find a product by id.\n'
+        '6. Find a product by name.\n'
+        '7. Show a product sales diagram.\n'
+        '8. Show a customer purchases diagram.\n'
+        '9. Show a product price changes diagram.\n'
+        '10. Change a product price.\n'
+        '0. Exit!'
     )
-    purchases_each_month = each_month(product=product_name, mall=shopping_mall)
-    # A list of purchases in last year
-    # Each index shows the month index
-    last_year_purchase = purchases_each_month
 
-    print(last_year_purchase)
-
-    create_purchase_diagram(
-        purchases=last_year_purchase,
-        time=[i for i in range(1, 13)]
-    )
-    plt.show()
+    order = int(input('Please enter your order: '))
+    match order:
+        case 1:
+            shopping_mall.show_customers()
+        case 2:
+            shopping_mall.show_products()
+        case 3:
+            shopping_mall.show_purchases()
+        case 4:
+            customer_id = int(input('Please enter a customer id: '))
+            user_customer = shopping_mall.find_customer_by_id(customer_id=customer_id)
+            print(user_customer)
+        case 5:
+            product_id = int(input('Please enter a product id: '))
+            user_product = shopping_mall.find_product_by_id(product_id=product_id)
+            print(user_product)
+        case 6:
+            product_name = input('Please enter a product name: ')
+            user_product = shopping_mall.find_product_by_name(product_name=product_name)
+            print(user_product)
+        case 7:
+            product_name = input('Please enter a product name: ')
+            user_product = shopping_mall.find_product_by_name(product_name=product_name)
+            purchases_each_month = each_month(product=user_product, mall=shopping_mall)
+            create_diagram(
+                purchases=purchases_each_month,
+                time=[i for i in range(1, 13)],
+                ylabel='Purchases'
+            )
+            plt.show()
+        case 8:
+            customer_id = int(input('Please enter a customer id: '))
+            user_customer = shopping_mall.find_customer_by_id(customer_id=customer_id)
+            customer_purchese = customer_purchases(customer=user_customer, mall=shopping_mall)
+            create_diagram(
+                purchases=customer_purchese,
+                time=[i for i in range(1, 13)],
+                ylabel='Customer purcheses'
+            )
+            plt.show()
+        case 9:
+            product_name = input('Please enter a product name: ')
+            user_product = shopping_mall.find_product_by_name(product_name=product_name)
+            price_changes = each_month_price(product=user_product)
+            create_diagram(
+                purchases=price_changes,
+                time=[i for i in range(1, 13)],
+                ylabel='Prices'
+            )
+            plt.show()
+        case 10:
+            pass
